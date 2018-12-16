5d6fc5d80e37cc2e34a0cfeedc5d4308f59697a7
Woongsik Choi <ws807.choi@samsung.com>
2018-12-16 16:42:13 +0900

    test

--- UNDEFINED_IMPORT1/src/export2.js
+++
@@ -1,3 +0,0 @@
-export const name13 = "name13";
-export const name17 = "name17";
-export const super01 = "super01";
--- UNDEFINED_IMPORT4/src/export2.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export const name13 = "name13";
-export const name17 = "name17";
-export const super01 = "super01";
