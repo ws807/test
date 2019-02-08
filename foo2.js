58c90d9c10d5157a05052afec7f9ebba1a06f7db
Woongsik Choi <ws807.choi@samsung.com>
2019-02-08 11:10:13 +0900

    Test

--- foo2.js
+++
@@ -1,4 +0,0 @@
-export default function foo() {
-  return null;
-}
-
---
+++ mjs1/export_dir/index.mjs
@@ -0,0 +1,10 @@
+/*******************************************************************************
+    Copyright (c) 2019, S-Core.
+    All rights reserved.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+  ******************************************************************************/
+
+export function foo3(x) { return x.prop3; }
