820c03ae350596e251bb59285b3307c4ee9d3a11
ws807 <41432352+ws807@users.noreply.github.com>
2026-06-03 21:57:55 +0900

    Update 06-03

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
 import a from 'aaa';
-import b from 'bbb';
-import c from 'ccc';
-import d from 'ddd';
-import e from 'eee';
-import f from 'fff';
 
 function foo() {
-  null.prop2
+  null.prop
 }  
